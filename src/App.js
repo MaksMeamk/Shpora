87ec957595899aa84b3a083f408084a695f537f3
Maks Matskevich <maks_memak@mail.ru>
2023-06-14 21:17:02 +0300

    navigation

--- src/App.js
+++ src/App.js
@@ -3,10 +3,18 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className="wrapper">
+      <header className="header">
+        <img src={logo} className="logo" alt="logo" />
       </header>
+      <main>
+        <nav className='menu'>
+          <ul className='menu__list'>
+            <li className='menu__item'></li>
+          </ul>
+        </nav>
+      </main>
+      <footer></footer>
     </div>
   );
 }
