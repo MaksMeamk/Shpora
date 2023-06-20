0edaeadb119a8c6273ba5f85010b5e4ef1265189
Maks Matskevich <maks_memak@mail.ru>
2023-06-20 22:20:18 +0300

    Day2

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
 import Navigation from './Components/Navigation';
 import MainPage from './Components/MainPage';
 import { HomeOutlined } from '@ant-design/icons'
@@ -11,17 +11,17 @@ function App() {
 
       <BrowserRouter>
         <header className="header">
-          <div className=''>
-            <HomeOutlined />
+          <div className='image-home'>
+            <Link className='image-home__link' to='/'>
+              <HomeOutlined className='image-home__item' />
+            </Link>
           </div>
-          <div className=''>
+          <div className='main-title'>
             Основная теория по библиотеке React
           </div>
-          <div className=''>
-            <img src={logo} className="logo" alt="logo" />
+          <div className='logo'>
+            <img src={logo} className="logo__item" alt="logo" />
           </div>
-
-
         </header>
         <main className='main'>
           <div className='main__navigation'>
@@ -48,7 +48,9 @@ function App() {
             </Routes>
           </div>
         </main>
-        <footer></footer>
+        <footer className='footer'>
+          <div className='footer__item'>Create app in 2023</div>
+        </footer>
       </BrowserRouter>
     </div >
   );
