958a77df02c9f9c70f83e63ecb4be2b116914e5e
Maks Matskevich <maks_memak@mail.ru>
2023-06-27 21:58:58 +0300

    day4

--- src/Components/Components.js
+++ src/Components/Components.js
@@ -7,19 +7,40 @@ const Components = () => {
             <div className='theme__title'> Компоненты </div>
             <div className='theme__text text'>
                 <p className='text-item'>Компоненты React — это многократно иcпользуемые фрагменты JavaScript-кода, которые выводят HTML-элементы (благодаря JSX ).</p>
+                <p className='text-item'>Компоненты позволяют разбить интерфейс на независимые части, про которые легко думать в отдельности.</p>
                 <p className='text-item'>Компоненты бывают функциональные и классовые.</p>
-                <p className='text-item'>Проще всего объявить React-компонент как функцию:</p>
             </div>
-            <div className='theme__code'>
-                <pre><code>{
-                    `function Welcome() {
+            <div className='theme__code code'>
+                <p className='code__title'>Проще всего объявить React-компонент как функцию:</p>
+                <pre className='code__item'><code>{
+                    `function FuncComponent() {
     return (
         <h1>Привет, мир!</h1>
     );
 }`}</code>
                 </pre>
             </div>
-        </div>
+            <div className='theme__code code'>
+                <p className='code__title'>Ещё компоненты можно определять как классы:</p>
+                <pre className='code__item'><code>{`class ClassComponent extends React.Component {
+    render() {
+        return (
+            <h1>Привет, мир!</h1>
+        )
+    }
+}`}</code>
+                </pre>
+            </div>
+
+        </div >
     )
 }
 export default Components;
+
+class ClassComponent extends React.Component {
+    render() {
+        return (
+            <h1>Привет, мир!</h1>
+        )
+    }
+}
