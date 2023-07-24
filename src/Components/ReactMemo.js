1a1f3615735c65a4a2220c6a9973c2e6658a448f
Maks Matskevich <maks_memak@mail.ru>
2023-07-24 21:53:13 +0300

    UseEffect

--- src/Components/ReactMemo.js
+++ src/Components/ReactMemo.js
@@ -9,22 +9,11 @@ const ReactMemo = () => {
                 <p className='text__item'>Если ваш компонент всегда рендерит одно и то же при неменяющихся пропсах, вы можете обернуть его в вызов React.memo для повышения производительности в некоторых случаях, мемоизируя тем самым результат. Это значит, что React будет использовать результат последнего рендера, избегая повторного рендеринга.</p>
                 <p className='text__item'>React.memo затрагивает только изменения пропсов. Если функциональный компонент обёрнут в React.memo и использует useState, useReducer или useContext, он будет повторно рендериться при изменении состояния или контекста.</p>
             </div>
-
-            <div className='theme__text text'>
-                <p className='text__item'></p>
-                <p className='text__item'></p>
-                <p className='text__item'></p>
-            </div>
-            <div className='theme__text text'>
-                <p className='text__item'></p>
-                <p className='text__item'></p>
-                <p className='text__item'></p>
-            </div>
             <div className=' theme__code code code'>
                 <p className='code__title'>По умолчанию он поверхностно сравнивает вложенные объекты в объекте props. Если вы хотите контролировать сравнение, вы можете передать свою функцию сравнения в качестве второго аргумента.</p>
                 <pre className='code__item'>
                     <code>{`function MyComponent(props) {
-                /* рендер с использованием пропсов */
+    /* рендер с использованием пропсов */
 }
 
 function areEqual(prevProps, nextProps) {
@@ -33,6 +22,19 @@ function areEqual(prevProps, nextProps) {
 }
 
 export default React.memo(MyComponent, areEqual);`}
+                    </code>
+                </pre>
+            </div>
+            <div className='theme__text text'>
+                <p className='text__item'>Этот метод предназначен только для оптимизации производительности.</p>
+            </div>
+
+            <div className=' theme__code code code'>
+                <p className='code__title'>Аналогом в классовых компонентах выступает метод shouldComponentUpdate()</p>
+                <pre className='code__item'>
+                    <code>{`shouldComponentUpdate(nextProps, nextState) {
+    return !shallowEqual (nextProps, this.props) || !shallowEqual (nextState, this.state);
+}`}
                     </code>
                 </pre>
             </div>
--- src/Components/UseEffect.js
+++ src/Components/UseEffect.js
@@ -3,16 +3,54 @@ import React from 'react'
 const UseEffect = () => {
     return (
         <div className='theme'>
-            <div className='theme__title'></div>
+            <div className='theme__title'>UseEffect</div>
             <div className='theme__text text'>
                 <p className='text__item'></p>
                 <p className='text__item'></p>
                 <p className='text__item'></p>
             </div>
             <div className=' theme__code code code'>
-                <p className='code__title'></p>
+                <p className='code__title'>Для использования методов жизненного цикла в функциональных компонентах предназначек хук useEffect.</p>
                 <pre className='code__item'>
-                    <code>{``}
+                    <code>{`import {useEffect} from 'react';`}
+                    </code>
+                </pre>
+            </div>
+            <div className='theme__text text'>
+                <p className='text__item text__item_padding'>Хук позволяет использовать 3 метода жизненного цикла:
+                    <ol type='1' className='text__item_list list'>
+                        <li className='list_item'>componentDidMount()</li>
+                        <li className='list_item'>componentDidUpdate(prevProps, prevState)</li>
+                        <li className='list_item'>componentWillUnmount()</li>
+                    </ol></p>
+            </div>
+            <div className=' theme__code code code'>
+                <p className='code__title'>UseEffect принимает в качестве первого параметра функцию, в качестве второго - [ ] (аналог componentDidMount).</p>
+                <pre className='code__item'>
+                    <code>{`useEffect ( () => { }, [])`}
+                    </code>
+                </pre>
+            </div>
+            <div className=' theme__code code code'>
+                <p className='code__title'>UseEffect принимает в качестве первого параметра функцию, в качестве второго - массив с зависимостью (или несколькими) (аналог componentDidUpdate).</p>
+                <pre className='code__item'>
+                    <code>{`useEffect ( () => { }, [name])`}
+                    </code>
+                </pre>
+            </div>
+            <div className=' theme__code code code'>
+                <p className='code__title'>UseEffect принимает в качестве первого параметра функцию, второго параметра - нет (аналог componentDidMount + componentDidUpdate).</p>
+                <pre className='code__item'>
+                    <code>{`useEffect ( () => { } )`}
+                    </code>
+                </pre>
+            </div>
+            <div className=' theme__code code code'>
+                <p className='code__title'>UseEffect принимает в качестве первого параметра функцию, которая возвращает другую функцию (аналог componentWillUnmount).</p>
+                <pre className='code__item'>
+                    <code>{`useEffect ( () => {
+    return () => {};
+})`}
                     </code>
                 </pre>
             </div>
